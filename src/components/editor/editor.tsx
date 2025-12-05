599d17516e3e318ca034ef24689e4a6999460501
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2025-12-05 20:55:00 +0100

    Added more blocks

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import { TrailingBlockPlugin } from "platejs";
 import { Plate, usePlateEditor } from "platejs/react";
 import { Editor, EditorContainer } from "../ui/editor";
 import { BasicBlocksKit } from "./plugins/basic-blocks-kit";
 import { BasicMarksKit } from "./plugins/basic-marks-kit";
+import { FloatingToolbarKit } from "./plugins/floating-toolbar-kit";
+import { LinkKit } from "./plugins/link-kit";
+import { ListKit } from "./plugins/list-kit";
+import { MathKit } from "./plugins/math-kit";
 
 export default function PlateEditor() {
   const editor = usePlateEditor({
     plugins: [
       // Elements
       ...BasicBlocksKit,
+      ...MathKit,
+      ...LinkKit,
+      ...ListKit,
+      TrailingBlockPlugin,
+
+      // UI
+      ...FloatingToolbarKit,
 
       // Marks
       ...BasicMarksKit,
